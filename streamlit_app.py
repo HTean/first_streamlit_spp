3325b5f3fe2d6637606e6cda56117f4fff40487a
Tean <97654099+HTean@users.noreply.github.com>
2022-07-26 11:19:02 -0400

    Update streamlit_app.py
    Changed all texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
-streamlit.title("This is my title")
-streamlit.text("This is my text")
-streamlit.text("Words above are stupid")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
